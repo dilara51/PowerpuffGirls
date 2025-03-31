20b0b112ff63fb9775be0f41d410813b33119c76
dilara51 <86346629+dilara51@users.noreply.github.com>
2025-03-31 21:09:55 +0300

    Models wurden hinzugefügt

---
+++ server/model/User.js
@@ -0,0 +1,24 @@
+class User {
+    constructor(id, email, password, firstName, lastName) {
+      this.id = id;
+      this.email = email;
+      this.password = password;
+      this.firstName = firstName;
+      this.lastName = lastName;
+      this.createdAt = new Date();
+      this.lastLogin = null;
+  
+      this.teams = []; // Beziehung zu Team
+      this.tasks = []; // Beziehung zu Task
+    }
+  
+    addTeam(team) {
+      this.teams.push(team);
+    }
+  
+    assignTask(task) {
+      this.tasks.push(task);
+    }
+  }
+  
+  module.exports = User;
